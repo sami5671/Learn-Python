eb758277851d26b7a15157a81c2a0456650f22e3
sami5671 <samialam5671@gmail.com>
2025-03-21 21:30:57 +0600

    Django template language, rendering html template using bootstrap

--- Learning-16/school_management/student/views.py
+++ Learning-16/school_management/student/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile(request):
-    return HttpResponse("<h1>Student profile</h1>")
+    user_data = {
+        "name": "SAMI ALAM",
+        "email": "samialam@gmail.com",
+        "age": 10,
+        "district": "dhaka",
+    }
+    # return HttpResponse("<h1>Student profile</h1>")
+    return render(request, "student/index.html", user_data)
 
 
 def home(request):
