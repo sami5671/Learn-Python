296bb2d8741d71c6edef5e027d92efb327df8d9b
sami5671 <samialam5671@gmail.com>
2025-06-28 15:12:47 +0600

    class based view CRUD technique done

--- Learn-Django/Student_management/student/urls.py
+++ Learn-Django/Student_management/student/urls.py
@@ -4,8 +4,18 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
-    path("create/", views.create_student, name="create_student"),
-    path("edit/<int:id>/", views.update_student, name="update_student"),
-    path("delete/<int:id>/", views.delete_student, name="delete_student"),
+    # path("home/", views.home, name="home"), #function based
+    path("home/", views.StudentLists.as_view(), name="home"),  # class based
+    # path("create/", views.create_student, name="create_student"),  # function based
+    path(
+        "create/", views.createStudent.as_view(), name="create_student"
+    ),  # function based
+    # path("edit/<int:id>/", views.update_student, name="update_student"),# function based
+    path(
+        "edit/<int:id>/", views.UpdateStudentData.as_view(), name="update_student"
+    ),  # class based
+    # path("delete/<int:id>/", views.delete_student, name="delete_student"),  # function based
+    path(
+        "delete/<int:id>/", views.DeleteStudent.as_view(), name="delete_student"
+    ),  # class based
 ]
--- Learn-Django/Student_management/student/views.py
+++ Learn-Django/Student_management/student/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from . import forms, models
 from .forms import StudentForm
@@ -43,11 +45,20 @@ from .forms import StudentForm
 # this for model form
 
 
+# For function based views
 def home(request):
     students = models.Student.objects.all()
     return render(request, "student/index.html", {"students": students})
 
 
+# For Class based views
+class StudentLists(ListView):
+    model = models.Student
+    template_name = "student/index.html"
+    context_object_name = "students"
+
+
+#  For function based views
 def create_student(request):
     if request.method == "POST":
         form = forms.StudentForm(request.POST, request.FILES)
@@ -57,6 +68,16 @@ def create_student(request):
     else:
         form = forms.StudentForm()
     return render(request, "student/create_student.html", {"form": form})
+
+
+# for class based view
+class createStudent(CreateView):
+    form_class = forms.StudentForm
+    template_name = "student/create_student.html"
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        return super().form_valid(form)
 
 
 def update_student(request, id):
@@ -71,7 +92,32 @@ def update_student(request, id):
     return render(request, "student/edit_student.html", {"form": form, "edit": True})
 
 
+# For Class based views
+class UpdateStudentData(UpdateView):
+    form_class = forms.StudentForm
+    model = models.Student
+    template_name = "student/edit_student.html"
+    success_url = reverse_lazy("home")
+    pk_url_kwarg = "id"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["edit"] = True
+        return context
+
+
 def delete_student(request, id):
     student = models.Student.objects.get(id=id)
     student.delete()
     return redirect("home")
+
+
+# For Class based views
+class DeleteStudent(DeleteView):
+    model = models.Student
+    pk_url_kwarg = "id"
+    success_url = reverse_lazy("home")
+    template_name = "student/delete_student.html"
+
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
