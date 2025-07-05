3f6ac312092aed0ac619fa8e245b73a137afa027
sami5671 <samialam5671@gmail.com>
2025-07-05 13:08:30 +0600

    authentication in Django(login, logout)

---
+++ Learning-18/Student_management/student/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from . import models
+
+
+class StudentForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = models.Student
+        fields = "__all__"
+        labels = {"name": "Full Name", "photo": "Upload Photo"}
+        help_texts = {"email": "Email will be confidential"}
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
