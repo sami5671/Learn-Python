1952e332acd226d0bc3b1d23b212f5747351311d
sami5671 <samialam5671@gmail.com>
2025-07-10 20:34:37 +0600

    postnest auth

--- 21. Full Funtional Blog Project (Django)/Django_Blog_Site/auths/views.py
+++ 21. Full Funtional Blog Project (Django)/Django_Blog_Site/auths/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+# calling the user model functions
+User = get_user_model()
+
+
+# creating new userCreating form object (because we have modified the User model ---> AbstractUser)
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2"]
+
 
 # Create your views here.
+
+
+# register and login automatically in any application
+def register_view(request):
+    if request.method == "POST":
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = MyUserCreationForm()
+
+    context = {"form": form}
+    return render(request, "auths/register.html", context)
