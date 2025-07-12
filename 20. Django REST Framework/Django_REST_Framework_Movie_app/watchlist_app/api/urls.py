31296fc3a6d21c08fb6a73d1de8f4162c53ce253
sami5671 <samialam5671@gmail.com>
2025-07-12 13:17:27 +0600

    CRUD operation done in REST framework

--- 20. Django REST Framework/Django_REST_Framework_Movie_app/watchlist_app/api/urls.py
+++ 20. Django REST Framework/Django_REST_Framework_Movie_app/watchlist_app/api/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("", views.movie_list),
-]
+urlpatterns = [path("", views.movie_list), path("<pk>/", views.movie_detail)]
